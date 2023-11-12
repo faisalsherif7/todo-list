439a6f43945a8f137e6da38b9147e81ae202f2f4
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-11-12 20:42:21 +0530

    Create factory functions to create projects and to do lists

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// Logic to create new project
+const createProject = function(title) {
+    const todos = [];
+    return { title, todos };
+}
+
+// Creating default project to be used
+const inbox = createProject('Inbox');
+
+// Factory function to create new todo list, added to inbox by default
+const createToDoItem = function(title, description, dueDate, priority, container = inbox) {
+
+    // Define a "project" property for every todo item that returns the title of the project it belongs to
+    const project = container.title
+
+    // Create the new to-do item
+    const createdItem = { title, description, dueDate, priority, project };
+
+    // Push the newly created item onto the desired projects list
+    container.todos.push(createdItem);
+
+    // Return the item, thereby assigning it to the created variable outside 
+    return createdItem;
+}
+
+// Creation of new todo item for testing
+const item = createToDoItem('item')
+console.log(item.project);
