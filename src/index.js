31d07d05963faf77ef53e8e01aad151de5a6fb22
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-11-18 20:29:40 +0530

    Create allProjects array that contains all projects and all todo lists

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
+const allProjects = [];
+
 // Factory function to create new project
 const createProject = function(title) {
     const todos = {};
-    return { title, todos };
+    const newProject = { title, todos };
+    allProjects.push(newProject);
+    return newProject;
 }
 
 // Creating default project to be used
 const inbox = createProject('Inbox');
 
 // Factory function to create new todo list, added to inbox by default
-const createToDoItem = function(title, container = inbox, description, dueDate, priority) {
+const createToDoItem = function(title, directory = inbox, description, dueDate, priority) {
 
     // Define a "project" method for every todo item that returns the title of the project it belongs to
     const project = function () {
-        return container.title;
+        return directory.title;
     }
 
     // Define a "complete" property which defaults to False
@@ -22,7 +26,7 @@ const createToDoItem = function(title, container = inbox, description, dueDate, 
     const createdItem = { title, description, dueDate, priority, project, complete };
 
     // Push the newly created item onto the desired projects list
-    container.todos[title] = createdItem;
+    directory.todos[title] = createdItem;
 
     // Return the item, thereby assigning it to the created variable outside 
     return createdItem;
@@ -36,3 +40,6 @@ const tasks = createProject('Tasks');
 const learn = createToDoItem('Learn');
 console.log(learn.project());
 console.log(tasks);
+
+
+console.log(allProjects);
