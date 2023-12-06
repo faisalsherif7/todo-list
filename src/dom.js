7f34caef2c1c18c80233ae23fd04f055595ce84b
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-12-06 11:58:22 +0530

    Remove navbar and style sidebar

--- src/dom.js
+++ src/dom.js
@@ -39,7 +39,7 @@ const taskDialog = function() {
 }();
 
 const projectDialog = function() {
-    const addTaskButton = document.querySelector('.add-project');
+    const addTaskButton = document.querySelector('.add-project-button');
     const closeDialogButton = document.querySelector('.close-project-dialog')
     const dialog = document.querySelector('.add-project-dialog');
     addTaskButton.addEventListener("click", () => {
@@ -61,7 +61,9 @@ function displayProjects() {
         newProject.setAttribute('data-project-id', `${project.id}`)
         newProject.innerHTML = `
             <p class="project-title">${project.title}</p>  
-            <button class="delete-project-button">del</button>
+            <p class="delete-project-button">
+            <svg class="delete-project-svg" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px"><path d="M 10.806641 2 C 10.289641 2 9.7956875 2.2043125 9.4296875 2.5703125 L 9 3 L 4 3 A 1.0001 1.0001 0 1 0 4 5 L 20 5 A 1.0001 1.0001 0 1 0 20 3 L 15 3 L 14.570312 2.5703125 C 14.205312 2.2043125 13.710359 2 13.193359 2 L 10.806641 2 z M 4.3652344 7 L 5.8925781 20.263672 C 6.0245781 21.253672 6.877 22 7.875 22 L 16.123047 22 C 17.121047 22 17.974422 21.254859 18.107422 20.255859 L 19.634766 7 L 4.3652344 7 z"/></svg>
+            </p>
         `;
         sidebar.appendChild(newProject);
     }
