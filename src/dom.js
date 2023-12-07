07e91f42f70e366ad0ddfc9623648fbdb7cc4b80
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-12-07 21:07:20 +0530

    Delete task function now gets id of task from parent div element

--- src/dom.js
+++ src/dom.js
@@ -120,12 +120,12 @@ export const displayTasks = function(tasksObject) {
     for (const key in tasksObject) {
         const task = tasksObject[key];
         content.innerHTML += `
-            <div class="task-item">
+            <div class="task-item" data-task-id="${task.id}">
                 <p class="task-title">Task name - ${task.title}</p>
                 <p class="task-description">Description - ${task.description}</p>
                 <p class="task-priority">Priority - ${task.priority}</p>
                 <p class="task-due-date">Due date - ${task.dueDate}</p>
-                <button type="button" class="delete-task-button" data-task-id="${task.id}">
+                <button type="button" class="delete-task-button">
                     <svg class="delete-task-svg" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px"><path  class="delete-project-svg-path" d="M 10.806641 2 C 10.289641 2 9.7956875 2.2043125 9.4296875 2.5703125 L 9 3 L 4 3 A 1.0001 1.0001 0 1 0 4 5 L 20 5 A 1.0001 1.0001 0 1 0 20 3 L 15 3 L 14.570312 2.5703125 C 14.205312 2.2043125 13.710359 2 13.193359 2 L 10.806641 2 z M 4.3652344 7 L 5.8925781 20.263672 C 6.0245781 21.253672 6.877 22 7.875 22 L 16.123047 22 C 17.121047 22 17.974422 21.254859 18.107422 20.255859 L 19.634766 7 L 4.3652344 7 z"/></svg>
                 </button>
             </div>
@@ -138,7 +138,7 @@ export const deleteTaskEvent = function() {
     content.addEventListener('click', (event) => {
         if (event.target.closest('.delete-task-button')) {
             const clickedDeleteButton = event.target.closest('.delete-task-button');
-            const id = clickedDeleteButton.dataset.taskId;
+            const id = clickedDeleteButton.parentElement.dataset.taskId;
             crud.deleteTask(id);
         }
     })
