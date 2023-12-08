7f0cb9eaa88a13afc6eee26ed4b9515b9c2d2f27
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-12-08 21:21:37 +0530

    Remove description from task item

--- src/dom.js
+++ src/dom.js
@@ -147,10 +147,9 @@ export const displayTasks = function(tasksObject) {
         const task = tasksObject[key];
         content.innerHTML += `
             <div class="task-item" data-task-id="${task.id}">
-                <p class="task-title">Task name - ${task.title}</p>
-                <p class="task-description">Description - ${task.description}</p>
-                <p class="task-priority">Priority - ${task.priority}</p>
-                <p class="task-due-date">Due date - ${task.dueDate}</p>
+                <p class="task-title">${task.title}</p>
+                <p class="task-due-date">${task.dueDate}</p>
+                <p class="task-priority-${task.priority}">${task.priority}</p>
                 <div class="action-buttons-container">
                     <div class="action-button edit-task-button">
                         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
