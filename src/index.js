6b54a479537a6469b8b984d367919fc5b6cdfc3a
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-12-14 16:35:17 +0530

    Initial dist subtree commit

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './style.css';
-import * as crud from './crud.js';
-import * as dom from './dom.js';
-
-crud.syncAllProjects();
-dom.displayProjects();
-dom.displayTasks();
-
-console.log(crud.allProjects);
